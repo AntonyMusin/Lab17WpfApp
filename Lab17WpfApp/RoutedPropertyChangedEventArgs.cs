395f3d187f5399aaae1ec200815d98ee159f60ae
Professional <Professional@USER-PC>
2021-12-28 21:48:54 +0300

    Добавьте файлы проекта.

---
+++ Lab17WpfApp/RoutedPropertyChangedEventArgs.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab17WpfApp
+{
+    internal class RoutedPropertyChangedEventArgs
+    {
+        internal readonly object NewValue;
+    }
+}
---
+++ Lab17WpfApp/txb.cs
@@ -0,0 +1,7 @@
+﻿namespace Lab17WpfApp
+{
+    internal class txb
+    {
+        internal static string Text;
+    }
+}
